f386c1e803e31a442d8b75ac2a8b5142e72ff8c8
jimmycloutier <scrooll@hotmail.com>
2017-10-12 21:27:32 -0400

    Start api structure

--- backend/project/controllers.py
+++ backend/project/controllers.py
@@ -1,7 +1,8 @@
-from flask import jsonify, request, abort
+from flask import jsonify, request, abort, url_for
 from . import project
 from models.projects import  Projects
 from models import db
+from functionalProcess import functionprocess
 
 #Test API route
 @project.route("/", methods=['GET'])
@@ -19,8 +20,8 @@ def get_all_projects():
 def get_projects(organization_id):
     """Get all projects related to an organization <organization_id>"""
     prjs = Projects.query.filter(Projects.organization_id == organization_id).all()
-    all_prjs = {'Projects' : [prj.to_json() for prj in prjs]}
-    return jsonify(all_prjs)
+    return jsonify({'Projects' : [prj.to_json() for prj in prjs]} if prjs else {'message': 'No projects found'}), 200 if prjs else 404
+
 
 @project.route("/v1.0/organizations/<organization_id>/projects", methods=['POST'])
 def create_project(organization_id):
@@ -41,7 +42,7 @@ def create_project(organization_id):
 def get_this_project(organization_id, project_id):
     """Get a specific projet <project_id>"""
     prj = Projects.query.filter(Projects.id == project_id).first()
-    return jsonify({'Projects': [prj.to_json()]} if prj else {'message': 'Project not found'}), 404
+    return jsonify({'Projects': [prj.to_json()]} if prj else {'message': 'Project not found'}), 200 if prj else 404
 
 @project.route("/v1.0/organizations/<organization_id>/projects/<project_id>", methods=['PUT'])
 def update_project(organization_id, project_id):
@@ -55,7 +56,7 @@ def update_project(organization_id, project_id):
         prj.projectName = request.json.get('Name', prj.projectName)
         db.session.commit()
 
-    return jsonify({'Projects': [prj.to_json()]} if prj else {'message': 'project not found'}), 404
+    return jsonify({'Projects': [prj.to_json()]} if prj else {'message': 'project not found'}), 200 if prj else 404
 
 
 @project.route("/v1.0/organizations/<organization_id>/projects/<project_id>", methods=['DELETE'])
@@ -66,8 +67,17 @@ def delete_project(organization_id, project_id):
     if prj:
         db.session.delete(prj)
         db.session.commit()
-        return jsonify({'message':'Project deleted successfully'})
+        return jsonify({'message':'Project deleted successfully'}), 200
     else:
         return jsonify({'message':'Project not found'}), 400
 
+@project.route("/v1.0/organizations/<organization_id>/projects/<project_id>/applypattern/<pattern_id>", methods=['GET'])
+def apply_pattern(organization_id, project_id, pattern_id):
+    """
+    Apply a pattern to a projet
+    JSON request can contain a list of datamovement names that need to be used for the project instead of the one
+    specified in the pattern
+    """
+    testlien = url_for('functionalprocess.test_fp_route')
+    return jsonify({'message': testlien}), 201
 
